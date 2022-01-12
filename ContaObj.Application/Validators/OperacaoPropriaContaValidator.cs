d78a3e3d58f95c8612190157f727a47dc74d9e07
marlon.almeida <marlon.almeida@NOTEMGA-ALMEIDA>
2022-01-12 11:52:49 -0300

    Adiciona validação para as operações na própria conta

---
+++ ContaObj.Application/Validators/OperacaoPropriaContaValidator.cs
@@ -0,0 +1,15 @@
+﻿using ContaObj.Domain.ViewModel.Conta;
+using FluentValidation;
+
+namespace ContaObj.Application.Validators
+{
+    public class OperacaoPropriaContaValidator : AbstractValidator<OperacaoPropriaConta>
+    {
+        public OperacaoPropriaContaValidator()
+        {
+            RuleFor(x => x.ContaId).NotNull().GreaterThan(0);
+            RuleFor(x => x.ValorTransacao).NotNull().GreaterThan(0);
+        }
+
+    }
+}
