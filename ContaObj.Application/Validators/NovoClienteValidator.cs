35abe0b7bed8d064ffa280d5211b58d6b9abcb20
Francis Silveira <francis.silveira@gmail.com>
2022-01-12 10:49:41 -0300

    Adiciona validações para a criação de um cliente.
    - Um cliente não pode ter menos de 18 anos
    - Um cliente não pode ter mais de 120 anos
    - O campo sexo deve ser M ou F

--- ContaObj.Application/Validators/NovoClienteValidator.cs
+++ ContaObj.Application/Validators/NovoClienteValidator.cs
@@ -1,5 +1,4 @@
-﻿
-using ContaObj.Domain.ViewModel;
+﻿using ContaObj.Domain.ViewModel;
 using FluentValidation;
 
 namespace ContaObj.Application.Validators
@@ -10,11 +9,16 @@ namespace ContaObj.Application.Validators
         {
             RuleFor(x => x.Nome).NotNull().NotEmpty().MinimumLength(6).MaximumLength(100);
             RuleFor(x => x.Documento).NotNull().NotEmpty().MinimumLength(9).MaximumLength(14);
-            RuleFor(x => x.DataNascimento).NotNull().NotEmpty().LessThan(DateTime.Now);
+            RuleFor(x => x.DataNascimento).NotNull().NotEmpty().GreaterThan(DateTime.Now.AddYears(-120)).LessThan(DateTime.Now.AddYears(-18));
             RuleFor(x => x.Telefones).NotNull().NotEmpty();
-            RuleFor(x => x.Sexo).NotNull();
+            RuleFor(x => x.Sexo).NotEmpty().Must(BeMOrF);
             RuleFor(x => x.Endereco).SetValidator(new NovoEnderecoValidator());
             RuleForEach(x => x.Telefones).SetValidator(new NovoTelefoneValidator());
+        }
+
+        private bool BeMOrF(char arg)
+        {
+            return arg is 'M' or 'F';
         }
     }
 }
