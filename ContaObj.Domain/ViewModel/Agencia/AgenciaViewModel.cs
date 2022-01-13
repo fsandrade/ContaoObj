cc0c55652dba0c3d18b023a06fb404fd745f5fa3
Francis Silveira <francis.silveira@gmail.com>
2022-01-13 15:48:13 -0300

    Adição das propriedades da ViewModel de Agência

--- ContaObj.Domain/ViewModel/Agencia/AgenciaViewModel.cs
+++ ContaObj.Domain/ViewModel/Agencia/AgenciaViewModel.cs
@@ -1,6 +1,9 @@
-﻿namespace ContaObj.Domain.ViewModel
+﻿namespace ContaObj.Domain.ViewModel.Agencia
 {
     public class AgenciaViewModel
     {
+        public int Numero { get; set; }
+        public string Nome { get; set; }
+        public EnderecoViewModel Endereco { get; set; }
     }
 }
