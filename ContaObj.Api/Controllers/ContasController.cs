72c8423513823651c93176421077bb5cf44890a6
Francis Silveira <francis.silveira@gmail.com>
2022-01-13 15:53:30 -0300

    Ajustes nos métodos de depositar e sacar para ficarem mais aderentes ao negócio
    - Removido o retorno booleano de depositar
    - Removida a necessidade de passagem do parâmetro tipo da transação.

--- ContaObj.Api/Controllers/ContasController.cs
+++ ContaObj.Api/Controllers/ContasController.cs
@@ -5,170 +5,146 @@ using ContaObj.Domain.ViewModel.Conta;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+namespace ContaObj.Api.Controllers;
 
-namespace ContaObj.Api.Controllers
+[Route("api/[controller]")]
+[ApiController]
+public class ContasController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ContasController : ControllerBase
+    private readonly IContaManager contaManager;
+
+    public ContasController(IContaManager contaManager)
     {
-        private readonly IContaManager contaManager;
+        this.contaManager = contaManager;
+    }
 
-        public ContasController(IContaManager contaManager)
+    [SwaggerOperation(Summary = "Realiza depósito em uma conta")]
+    [HttpPost("Depositar")]
+    public async Task<IActionResult> Depositar([SwaggerParameter("Dados do depósito")] OperacaoPropriaConta deposito)
+    {
+        try
         {
-            this.contaManager = contaManager;
+            await contaManager.DepositarAsync(deposito);
+        }
+        catch (ApplicationException ex)
+        {
+            return NotFound();
+        }
+        return Ok();
+    }
+
+    [SwaggerOperation(Summary = "Consulta transações da conta por período")]
+    [HttpGet("ExtratoPorPeriodo")]
+    public async Task<ActionResult<IEnumerable<TransacaoViewModel>>> ExtratoPorPeriodo(
+        [SwaggerParameter("Id da conta")] int contaId,
+        [SwaggerParameter("Início do período")] DateTime inicio,
+        [SwaggerParameter("Fim do período")] DateTime fim)
+    {
+        var transacoesNoPeriodo = await contaManager.ExtratoPorPeriodoAsync(contaId, inicio, fim);
+
+        if (transacoesNoPeriodo == null)
+        {
+            return NotFound();
         }
 
-        [SwaggerOperation(Summary = "Realiza depósito em uma conta")]
-        [HttpPut("Depositar")]
-        public async Task<ActionResult<bool?>> Depositar([SwaggerParameter("Dados do depósito")] OperacaoPropriaConta deposito)
+        return Ok(transacoesNoPeriodo);
+    }
+
+    [SwaggerOperation(Summary = "Consulta conta específica")]
+    [HttpGet("{contaId}")]
+    public async Task<ActionResult<ContaViewModel>> Get([SwaggerParameter("Id da conta")] int contaId)
+    {
+        var contaConsultada = await contaManager.GetContaAsync(contaId);
+
+        if (contaConsultada == null)
         {
-            if(deposito.TipoTransacao != TipoTransacao.Deposito)
-            {
-                return BadRequest(
-                    new ProblemDetails
-                    {
-                        Title = "One or more validation errors occurred.",
-                        Status = 400,
-                        Detail = "O tipo de transação informado é inválido."
-                    });
-            }
-
-            var depositoRealizado = await contaManager.DepositarAsync(deposito.ContaId, deposito.ValorTransacao);
-
-            if (depositoRealizado == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(depositoRealizado);
+            return NotFound();
         }
 
-        [SwaggerOperation(Summary = "Consulta transações da conta por período")]
-        [HttpGet("ExtratoPorPeriodo")]
-        public async Task<ActionResult<IEnumerable<TransacaoViewModel>>> ExtratoPorPeriodo(
-            [SwaggerParameter("Id da conta")] int contaId,
-            [SwaggerParameter("Início do período")] DateTime inicio,
-            [SwaggerParameter("Fim do período")] DateTime fim)
+        return Ok(contaConsultada);
+    }
+
+    [SwaggerOperation(Summary = "Consulta todas as contas")]
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ContaViewModel>>> Get()
+    {
+        var contasExistentes = await contaManager.GetContasAsync();
+
+        if (contasExistentes == null)
         {
-            var transacoesNoPeriodo = await contaManager.ExtratoPorPeriodoAsync(contaId, inicio, fim);
-
-            if (transacoesNoPeriodo == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(transacoesNoPeriodo);
+            return NotFound();
         }
 
-        [SwaggerOperation(Summary = "Consulta conta específica")]
-        [HttpGet("{contaId}")]
-        public async Task<ActionResult<ContaViewModel>> Get([SwaggerParameter("Id da conta")] int contaId)
+        return Ok(contasExistentes);
+    }
+
+    [SwaggerOperation(Summary = "Consulta todas as contas de um cliente")]
+    [HttpGet("ContasPorCliente/{clienteId}")]
+    public async Task<ActionResult<IEnumerable<ContaViewModel>>> ContasPorCliente([SwaggerParameter("Id do cliente")] int clienteId)
+    {
+        var contasDoCliente = await contaManager.GetContasPorClienteAsync(clienteId);
+
+        if (contasDoCliente == null)
         {
-            var contaConsultada = await contaManager.GetContaAsync(contaId);
-
-            if (contaConsultada == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(contaConsultada);
+            return NotFound();
         }
 
-        [SwaggerOperation(Summary = "Consulta todas as contas")]
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<ContaViewModel>>> Get()
+        return Ok(contasDoCliente);
+    }
+
+    [SwaggerOperation(Summary = "Inativa uma conta específica")]
+    [HttpDelete("{contaId}")]
+    public async Task<IActionResult> InativarConta([SwaggerParameter("Id da conta")] int contaId)
+    {
+        await contaManager.InativarContaAsync(contaId);
+
+        return Ok();
+    }
+
+    [SwaggerOperation(Summary = "Insere nova conta")]
+    [HttpPost]
+    public async Task<ActionResult<ContaViewModel>> InsertConta([SwaggerParameter("Conta para inserção")] NovaConta novaConta)
+    {
+        ContaViewModel contaInserida = await contaManager.InsertContaAsync(novaConta);
+        return CreatedAtAction(nameof(Get), new { id = contaInserida.Id }, contaInserida);
+    }
+
+    [SwaggerOperation(Summary = "Realiza saque em uma conta")]
+    [HttpPut("Sacar")]
+    public async Task<ActionResult<bool?>> Sacar([SwaggerParameter("Id da conta")] OperacaoPropriaConta saque)
+    {
+        var saqueRealizado = await contaManager.SacarAsync(saque.ContaId, saque.Valor);
+
+        if (saqueRealizado == null)
         {
-            var contasExistentes = await contaManager.GetContasAsync();
-
-            if (contasExistentes == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(contasExistentes);
+            return NotFound();
         }
 
-        [SwaggerOperation(Summary = "Consulta todas as contas de um cliente")]
-        [HttpGet("ContasPorCliente/{clienteId}")]
-        public async Task<ActionResult<IEnumerable<ContaViewModel>>> ContasPorCliente([SwaggerParameter("Id do cliente")] int clienteId)
+        return Ok(saqueRealizado);
+    }
+
+    [SwaggerOperation(Summary = "Altera conta")]
+    [HttpPut("{contaId}")]
+    public async Task<ActionResult<bool?>> UpdateConta([SwaggerParameter("Id da conta")] int contaId, [SwaggerParameter("Conta para alteração")] AlteraConta alteraConta)
+    {
+        if (contaId != alteraConta.Id)
         {
-            var contasDoCliente = await contaManager.GetContasPorClienteAsync(clienteId);
-
-            if (contasDoCliente == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(contasDoCliente);
+            return BadRequest(
+                new ProblemDetails
+                {
+                    Title = "One or more validation errors occurred.",
+                    Status = 400,
+                    Detail = "O Id informado no path é diferente do Id informado no body."
+                });
         }
 
-        [SwaggerOperation(Summary = "Inativa uma conta específica")]
-        [HttpDelete("{contaId}")]
-        public async Task<IActionResult> InativarConta([SwaggerParameter("Id da conta")] int contaId)
-        {
-            await contaManager.InativarContaAsync(contaId);
+        var alterouConta = await contaManager.UpdateContaAsync(alteraConta);
 
-            return Ok();
+        if (alterouConta == null)
+        {
+            return NotFound();
         }
 
-        [SwaggerOperation(Summary = "Insere nova conta")]
-        [HttpPost]
-        public async Task<ActionResult<ContaViewModel>> InsertConta([SwaggerParameter("Conta para inserção")] NovaConta novaConta)
-        {
-            ContaViewModel contaInserida = await contaManager.InsertContaAsync(novaConta);
-            return CreatedAtAction(nameof(Get), new { id = contaInserida.Id }, contaInserida);
-        }
-
-        [SwaggerOperation(Summary = "Realiza saque em uma conta")]
-        [HttpPut("Sacar")]
-        public async Task<ActionResult<bool?>> Sacar([SwaggerParameter("Id da conta")] OperacaoPropriaConta saque)
-        {
-            if (saque.TipoTransacao != TipoTransacao.Saque)
-            {
-                return BadRequest(
-                    new ProblemDetails
-                    {
-                        Title = "One or more validation errors occurred.",
-                        Status = 400,
-                        Detail = "O tipo de transação informado é inválido."
-                    });
-            }
-
-            var saqueRealizado = await contaManager.SacarAsync(saque.ContaId, saque.ValorTransacao);
-
-            if (saqueRealizado == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(saqueRealizado);
-        }
-
-        [SwaggerOperation(Summary = "Altera conta")]
-        [HttpPut("{contaId}")]
-        public async Task<ActionResult<bool?>> UpdateConta([SwaggerParameter("Id da conta")] int contaId, [SwaggerParameter("Conta para alteração")] AlteraConta alteraConta)
-        {
-            if (contaId != alteraConta.Id)
-            {
-                return BadRequest(
-                    new ProblemDetails
-                    {
-                        Title = "One or more validation errors occurred.",
-                        Status = 400,
-                        Detail = "O Id informado no path é diferente do Id informado no body."
-                    });
-            }
-
-            var alterouConta = await contaManager.UpdateContaAsync(alteraConta);
-
-            if (alterouConta == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(alterouConta);
-        }
+        return Ok(alterouConta);
     }
 }
