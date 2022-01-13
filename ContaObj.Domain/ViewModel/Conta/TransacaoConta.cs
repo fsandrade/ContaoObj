d8c1097e01e8ff61a9c0b7a6c60ac48cfe86483a
Francis Silveira <francis.silveira@gmail.com>
2022-01-13 15:46:17 -0300

    Remoção do tipo de transação ao depositar/sacar, o dado será inferido pelo método.

--- ContaObj.Domain/ViewModel/Conta/TransacaoConta.cs
+++ ContaObj.Domain/ViewModel/Conta/TransacaoConta.cs
@@ -4,17 +4,10 @@ namespace ContaObj.Domain.ViewModel.Conta
 {
     public class TransacaoConta
     {
-
         /// <summary>
         /// Valor da transação
         /// </summary>
         /// <example>10.00M</example>
-        public decimal ValorTransacao { get; set; }
-
-        /// <summary>
-        /// Tipo da transação
-        /// </summary>
-        /// <example>Saque</example>
-        public TipoTransacao TipoTransacao { get; set; }
+        public decimal Valor { get; set; }
     }
 }
