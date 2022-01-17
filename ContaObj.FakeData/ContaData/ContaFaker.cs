141a0cca0ecfce10fad36c08409f6ffe391b87f8
marlon.almeida <marlon.almeida@NOTEMGA-ALMEIDA>
2022-01-17 13:39:26 -0300

    Criação novo construtor contafaker com cliente e agência

--- ContaObj.FakeData/ContaData/ContaFaker.cs
+++ ContaObj.FakeData/ContaData/ContaFaker.cs
@@ -14,11 +14,23 @@ namespace ContaObj.FakeData.ContaData
             var saldo = new Faker().Random.Decimal(1, 99999);
             var limite = new Faker().Random.Decimal(1, 99999);
             RuleFor(p => p.Id, f => id);
-            RuleFor(p => p.Numero, f => id);
             RuleFor(p => p.Saldo, f => saldo);
             RuleFor(p => p.Limite, f => limite);
             RuleFor(p => p.Cliente, f => new ClienteFaker().Generate());
             RuleFor(p => p.Agencia, f => new AgenciaFaker().Generate());
+            RuleFor(p => p.Status, f => f.PickRandom<StatusConta>());
+        }
+
+        public ContaFaker(Cliente cliente, Agencia agencia)
+        {
+            var id = new Faker().Random.Number(1, 999999);
+            var saldo = new Faker().Random.Decimal(1, 99999);
+            var limite = new Faker().Random.Decimal(1, 99999);
+            RuleFor(p => p.Id, f => id);
+            RuleFor(p => p.Saldo, f => saldo);
+            RuleFor(p => p.Limite, f => limite);
+            RuleFor(p => p.Cliente, f => cliente);
+            RuleFor(p => p.Agencia, f => agencia);
             RuleFor(p => p.Status, f => f.PickRandom<StatusConta>());
         }
     }
