d4dd9862c3bbb460a8573486b0cef66fabb80886
marlon.almeida <marlon.almeida@NOTEMGA-ALMEIDA>
2022-01-17 11:47:36 -0300

    Criação mock para operação na própria conta

---
+++ ContaObj.FakeData/ContaData/OperacaoPropriaContaFaker.cs
@@ -0,0 +1,22 @@
+﻿using Bogus;
+using ContaObj.Domain.ViewModel.Conta;
+
+namespace ContaObj.FakeData.ContaData
+{
+    public class OperacaoPropriaContaFaker : Faker<OperacaoPropriaConta>
+    {
+
+        public OperacaoPropriaContaFaker(int contaId)
+        {
+            var valor = new Faker().Random.Decimal(1, 9999);
+            RuleFor(p => p.ContaId, f => contaId);
+            RuleFor(p => p.Valor, f => valor);
+        }
+
+        public OperacaoPropriaContaFaker(int contaId, decimal valor)
+        {
+            RuleFor(p => p.ContaId, f => contaId);
+            RuleFor(p => p.Valor, f => valor);
+        }
+    }
+}
