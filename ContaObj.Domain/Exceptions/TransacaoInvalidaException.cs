98601f449538524889262a4c09b57031a5c0b962
Francis Silveira <francis.silveira@gmail.com>
2022-01-20 16:11:34 -0300

    Adição da custom exception Transação inválida

---
+++ ContaObj.Domain/Exceptions/TransacaoInvalidaException.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace ContaObj.Domain.Exceptions;
+
+[Serializable]
+public class TransacaoInvalidaException : Exception
+{
+    public TransacaoInvalidaException()
+    { }
+
+    public TransacaoInvalidaException(string message)
+        : base(message) { }
+
+    public TransacaoInvalidaException(string message, Exception inner)
+        : base(message, inner) { }
+
+    protected TransacaoInvalidaException(SerializationInfo info, StreamingContext context)
+        : base(info, context) { }
+}
