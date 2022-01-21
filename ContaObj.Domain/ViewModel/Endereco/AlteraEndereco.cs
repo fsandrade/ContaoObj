7c1e30e9a787b3ef3176ad8c30bb35401c352eb2
marlon.almeida <marlon.almeida@NOTEMGA-ALMEIDA>
2022-01-21 13:30:03 -0300

    Criação altera endereço
    - para melhoria na legibilidade do código
    - estava causando erro no momento de atualizar cliente

---
+++ ContaObj.Domain/ViewModel/Endereco/AlteraEndereco.cs
@@ -0,0 +1,17 @@
+﻿namespace ContaObj.Domain.ViewModel.Endereco
+{
+    public class AlteraEndereco
+    {
+        /// <summary>
+        /// Id do endereço
+        /// </summary>
+        /// <example>1</example>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// CEP do endereço
+        /// </summary>
+        /// <example>15248965</example>
+        public string Cep { get; set; }
+    }
+}
